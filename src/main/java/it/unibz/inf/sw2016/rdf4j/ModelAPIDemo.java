a7cd6cd1887a8493702949771572eecc4f58cd30
Guohui Xiao <gh.xiao@gmail.com>
2016-11-08 09:16:41 +0100

    java 8

--- src/main/java/it/unibz/inf/sw2016/rdf4j/ModelAPIDemo.java
+++ src/main/java/it/unibz/inf/sw2016/rdf4j/ModelAPIDemo.java
@@ -2,9 +2,13 @@ package it.unibz.inf.sw2016.rdf4j;
 
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.LinkedHashModel;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.vocabulary.FOAF;
+import org.eclipse.rdf4j.model.vocabulary.RDF;
 
 public class ModelAPIDemo {
     public static void main(String[] args) {
@@ -14,6 +18,13 @@ public class ModelAPIDemo {
         IRI name = factory.createIRI("http://example.org/name");
         Literal bobsName = factory.createLiteral("Bob");
         Statement nameStatement = factory.createStatement(bob, name, bobsName);
-        System.out.println(nameStatement);
+        Statement typeStatement = factory.createStatement(bob, RDF.TYPE, FOAF.PERSON);
+
+
+        Model model = new LinkedHashModel();
+        model.add(typeStatement);
+        model.add(bob, name, bobsName);
+
+        model.forEach(System.out::println);
     }
 }
