4d1b20208b63b4ebedfc51c38fb91e7eaeecebc0
Guohui Xiao <gh.xiao@gmail.com>
2016-11-08 09:11:23 +0100

    initial imports

---
+++ src/main/java/it/unibz/inf/sw2016/rdf4j/ModelAPIDemo.java
@@ -0,0 +1,19 @@
+package it.unibz.inf.sw2016.rdf4j;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Literal;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+
+public class ModelAPIDemo {
+    public static void main(String[] args) {
+
+        ValueFactory factory = SimpleValueFactory.getInstance();
+        IRI bob = factory.createIRI("http://example.org/bob");
+        IRI name = factory.createIRI("http://example.org/name");
+        Literal bobsName = factory.createLiteral("Bob");
+        Statement nameStatement = factory.createStatement(bob, name, bobsName);
+        System.out.println(nameStatement);
+    }
+}
